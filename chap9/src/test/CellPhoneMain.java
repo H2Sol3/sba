fe144cb341eec18c092e3180cdae912193525019
user <thfastang401@naver.com>
2022-12-16 18:07:47 +0900

    22.12.16 자바 chap9 예제 및 문제 풀이(랜덤함수, 스트링버퍼, 문제!!!!!!!)

--- chap9/src/test/CellPhoneMain.java
+++ chap9/src/test/CellPhoneMain.java
@@ -25,22 +25,23 @@ class CellPhone {
 
 	void call(int min) throws IllegalArgumentException {
 			if (min < 0) {
-				throw new IllegalArgumentException("통화시간입력오류");
+				throw new IllegalArgumentException("통화시간입력오류"); //예외처리
 			}
 			
-			System.out.println("통화 시간 : " + min + "분");
+			System.out.println("통화 시간 : " + min + "분"); //예외가 발생하면 이 문장부터는 실행X, 예외 없으니 실행됨. 조건문 필요없다!!!
 			battery = battery - min * 0.5; // 배터리 감소	
 
-			if(battery<0) {
+			if(battery<0) { //배터리 -일때 0으로
 				battery=0;
 			}
 	}
-
+ 
 	// 메서드2
 	void charge(int time) throws IllegalArgumentException{ //time : 충전시간
 			if(time < 0) { //충전시간이 0보다 작을때
-				throw new IllegalArgumentException("충전시간입력오류");				
+				throw new IllegalArgumentException("충전시간입력오류"); //예외처리		
 			}
+			
 			System.out.println("충전 시간 : "+time +"분");
 			battery+=time*3;
 			
@@ -50,13 +51,15 @@ class CellPhone {
 	}
 
 	// 메서드3
-	void printBattery() {
+	void printBattery() { //배터리 양 출력만. 초기화 등등은 다른 함수에서 처리
 			System.out.println("남은 배터리 양 : " + battery);
 	}
 
 	// 메서드4
 	boolean isSame(CellPhone other) {
-		if(model.equalsIgnoreCase(String.valueOf(other.model))) {		
+		//model과 other를 비교하는건 model-GALAXY와 CellPhone 객체 전체를 비교하는 것. 북북북 처럼 CellPhone객체의 어떤 것을 가져와서
+		//비교해야하는지 정확하게ㅐ 찝어주기. model과 other.model 비교!!!!!!!!!!!!!!!!!
+		if(model.equalsIgnoreCase(String.valueOf(other.model))) {  
 			return true;
 		}else {
 			return false;
@@ -68,6 +71,7 @@ public class CellPhoneMain {
 	public static void main(String[] args) {
 
 		CellPhone myPhone = new CellPhone("GALAXY-7");
+		//CellPhone클래스에서 예외를 던졌으니 main에서 예외처리해주기
 		try {
 			myPhone.charge(20); // 20분간 충전을 한다.
 			myPhone.printBattery();
@@ -79,7 +83,7 @@ public class CellPhoneMain {
 			myPhone.printBattery();
 			myPhone.call(-20); // 통화시간입력오류			
 		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
+			System.out.println(e.getMessage()); //오류생기면 메세지 출력
 		}
 		CellPhone yourPhone = new CellPhone("galaxy-7");
 		if (myPhone.isSame(yourPhone)) {
